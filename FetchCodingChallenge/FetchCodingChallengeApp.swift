edd2fcb0c93be24f36c2e547dbdd7235834a4193
Vishnu Sriram <vsriram42@gmail.com>
2023-06-30 17:16:59 -0400

    Initial Commit

---
+++ FetchCodingChallenge/FetchCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchCodingChallengeApp.swift
+//  FetchCodingChallenge
+//
+//  Created by Vishnu Sriram on 6/30/23.
+//
+
+import SwiftUI
+
+@main
+struct FetchCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
